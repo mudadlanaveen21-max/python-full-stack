12b8a67384b2b45ad6e245b945575a894f285664
mudadlanaveen21-max <mudadlanaveen21@gmail.com>
2026-05-13 15:35:30 +0530

    today i learn abot what is python, keywords, identifiers, varibles

---
+++ Day1.py
@@ -0,0 +1,40 @@
+print("this is my day in python")
+'''
+what is python
+-high level language
+---->no need to worry about low level management(storage)
+-Dynamically typed language
+---->no need to mention the kind of data taking to variable
+-interperter language
+'''
+--->python will execute the code line by line and can through the error if any but all the before lines will execute and gives output
+Exanmple :
+    num = 23
+
+    any = [1,2]
+    an =["Name": "Naveen"]
+    print(num)
+    so =python
+    print(any)
+    print(an)
+
+TOKENS
+-------
+----> kEYWORDS in the program
+      eg : if
+           else
+           while
+           for
+IDENTIFIERS
+-----------
+----->name of  varibles, function and classes
+
+VARIBLES
+--------
+----> good ways = A-Z, a-z
+----> bad ways = special char, space, 0-9
+    eg: a=90
+        A=9
+        b8=89
+
+'''
